980c3eea8ee865b95f1c5d69460b04ee7ab8e9b4
NFD96 <n.f.danut@gmail.com>
2017-08-31 16:17:42 +0300

    App

---
+++ MagicSaloons/app/src/main/java/com/example/danutneagu/magicsaloons/ListViewImage.java
@@ -0,0 +1,63 @@
+package com.example.danutneagu.magicsaloons;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/*
+First Attempt
+Neagu Danut
+ */
+
+
+public class ListViewImage extends AppCompatActivity {
+
+    ListView listView;
+    Context context;
+    ArrayList progList;
+
+    public static Integer[] progImages = {
+      R.drawable.coafor,R.drawable.manichiura,R.drawable.cosmetica,R.drawable.tratamente,R.drawable.epilare,
+            R.drawable.machiajsemi,R.drawable.pmachiaj,R.drawable.punghii,R.drawable.ppar,R.drawable.logo
+    };
+
+    public static String[] progNames = {
+            "Coafor", "Manichiura", "Cosmetica", "Tratamente Academie", "Epilare Definitiva", "Machiaj Semipermanent"
+            , "Produse Machiaj", "Produse Unghii", "Produse Par", "Scrolling Test"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_image);
+
+        MyListAdapter adapter = new MyListAdapter(this, progNames, progImages);
+        listView = (ListView)findViewById(R.id.listView2);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListViewImage.this, " You clicked at " + progNames[position],Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), CoaforServicesList.class);
+                startActivityForResult(intent, RESULT_OK);
+                finish();
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the LoginActivity
+//        moveTaskToBack(true);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivityForResult(intent, RESULT_OK);
+        finish();
+    }
+
+}
---
+++ MagicSaloons/app/src/main/java/com/example/danutneagu/magicsaloons/LoginActivity.java
@@ -0,0 +1,175 @@
+package com.example.danutneagu.magicsaloons;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telecom.Connection;
+import android.util.Log;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.SQLException;
+
+import butterknife.ButterKnife;
+import butterknife.Bind;
+
+/*
+First Attempt
+Neagu Danut
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @Bind(R.id._emailText) EditText _emailText;
+    @Bind(R.id._passwordText) EditText _passwordText;
+    @Bind(R.id._loginButton) Button _loginButton;
+    @Bind(R.id._signupLink) TextView _signupLink;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+//      Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+
+            }
+        });
+
+        _signupLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.Theme_AppCompat_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful SignupActivity logic here
+                // By default we just finish the Activity and log them in automatically
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the LoginActivity
+        moveTaskToBack(true);
+//        super.onBackPressed();
+
+
+
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+//        java.sql.Connection conn = null;
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+//        if (conn != null) {
+//            try {
+//                conn.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        return valid;
+    }
+}
+
